0072d09aef8d36973945d14ae3319d30ebb0325e
Tal Gold <talpogorelis@gmail.com>
2021-02-18 15:58:05 +0200

    Added: Picsum fetch request

---
+++ src/requests.js
@@ -0,0 +1,9 @@
+const API_URL = "https://picsum.photos/v2/list";
+
+export const fetchPicturesData = async () => {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+
+  console.log(data);
+  return data;
+};
