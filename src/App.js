99fc30eb42a4646a6bb97238addf930e8b7d5743
Tal Gold <talpogorelis@gmail.com>
2021-02-18 16:29:34 +0200

    Added: Mapping imgs to basic react elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import { fetchPicturesData } from "./requests";
 
 function App() {
-  return (
-    <div className="App">
-      <div>Selected Picture</div>
-      <div>Picutre List</div>
-    </div>
-  );
+  const [imgs, setImgs] = useState(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    async function fetchImageData() {
+      const imgList = await fetchPicturesData();
+      setImgs(imgList);
+      setloading(false);
+    }
+    fetchImageData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <div>Selected Picture</div>
+        <div>Image list</div>
+        <div>
+          {imgs.map((picture) => {
+            return (
+              <div key={picture.id}>
+                <h3>Image number:{picture.id}</h3>
+                <p>{picture.author}</p>
+                <p>{picture.download_url}</p>
+                <p>
+                  {picture.height},{picture.width}
+                </p>
+                <p>{picture.url}</p>
+                <br />
+                <br />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,5 @@ const API_URL = "https://picsum.photos/v2/list";
 export const fetchPicturesData = async () => {
   const res = await fetch(API_URL);
   const data = await res.json();
-
-  console.log(data);
   return data;
 };
